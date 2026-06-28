adec8ac05ad39a013d12210e01de425fd2798166
louie pillari <loupillari@gmail.com>
2026-06-28 18:18:16 -0400

    add dbt task to airflow dag

--- dags/ingest_dag.py
+++ dags/ingest_dag.py
@@ -1,9 +1,13 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
 from datetime import datetime, timezone
 
-import sys
-sys.path.insert(0, '../src')
+# Config paths
+DBT_PROJECT_DIR = "/opt/airflow/dbt"
+DBT_PROFILES_DIR = "/opt/airflow/dbt"
+
+
 from main_ingest import run_ingest
 
 with DAG(
@@ -18,3 +22,13 @@ with DAG(
         task_id = "run_ingest",
         python_callable = run_ingest
     )
+
+    dbt_run = BashOperator(
+        task_id = "dbt_run",
+        bash_command = f"cd {DBT_PROJECT_DIR} && dbt build --profiles-dir {DBT_PROFILES_DIR}",
+
+    )
+
+
+    # Set task dependencies
+    ingest_task >> dbt_run
