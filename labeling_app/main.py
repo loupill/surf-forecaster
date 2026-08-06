aa4365aab14b9449e98978ccc504ae6b4fa40acb
louie pillari <loupillari@gmail.com>
2026-08-05 21:25:02 -0400

    add post method to main app

--- labeling_app/main.py
+++ labeling_app/main.py
@@ -1,5 +1,5 @@
 
-from fastapi import FastAPI, Depends, HTTPException, Request
+from fastapi import FastAPI, Depends, HTTPException, Request, Form
 from fastapi.staticfiles import StaticFiles
 from sqlalchemy import text
 from sqlalchemy.orm import Session
@@ -35,4 +35,65 @@ def get_rating_form(request: Request, token: str, db: Session=Depends(get_db)):
             "break_id": session_row.break_id,
             "session_date": session_row.session_date
         }
+    )
+
+
+@app.post("/rate/{token}")
+def post_rating(
+    token: str,
+    request: Request,    
+    morning_rating: int = Form(...),
+    morning_comment: str = Form(""),
+    midday_rating: int = Form(...),
+    midday_comment: str = Form(""),
+    evening_rating: int = Form(...),
+    evening_comment: str = Form(""),
+    db: Session = Depends(get_db),
+
+):    
+
+    result = db.execute(
+        text("select id from gold.labeling_sessions where token = :token"),
+        {"token": token}
+    )
+
+    session_id_row = result.fetchone()
+
+    if session_id_row is None:
+        return templates.TemplateResponse(
+            request=request,
+            name="error.html",
+            context={"token": token}
+        )
+
+    # UPDATE INSERT STATEMENTS - NEED ONE PER SESSION (MORNING MIDDAY EVENING)
+
+    
+    insert_morning = db.execute(
+        text("insert into gold.human_labels (session_id, time_block, rating, comment) values (:session_id, 'morning', :rating, :comments)"),
+        {"session_id": session_id_row.id, "rating": morning_rating, "comments": morning_comment}
+    ) 
+
+
+    insert_midday = db.execute(
+        text("insert into gold.human_labels (session_id, time_block, rating, comment) values (:session_id, 'midday', :rating, :comments)"),
+        {"session_id": session_id_row.id, "rating": midday_rating, "comments": midday_comment}
+    ) 
+
+    insert_evening = db.execute(
+        text("insert into gold.human_labels (session_id, time_block, rating, comment) values (:session_id, 'evening', :rating, :comments)"),
+        {"session_id": session_id_row.id, "rating": evening_rating, "comments": evening_comment}
+    ) 
+
+    db.execute(
+        text("update gold.labeling_sessions set responded_at = now() where id = :session_id"),
+        {"session_id": session_id_row.id}
+    )
+
+    db.commit()
+
+    return templates.TemplateResponse(
+        request=request,
+        name="success.html",
+        context={}
     )
