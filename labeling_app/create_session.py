c3585d7fdf0530da0c31dafefdd13771ce5f0132
louie pillari <loupillari@gmail.com>
2026-08-17 18:43:32 -0400

    update to add logging

--- labeling_app/create_session.py
+++ labeling_app/create_session.py
@@ -1,14 +1,14 @@
-# Create a token to use in our discord generated link
-
-#sql alchemy to increment token on each airflow run
-# Can we reuse our engine or do we need to create new
-
-
+import logging
 import secrets
 from datetime import date, datetime
 from sqlalchemy import text
 from ingest.db import get_engine
 
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s",
+)
 
 BREAK_ID = 'belmar'
 
@@ -17,20 +17,28 @@ def create_session():
     token = secrets.token_urlsafe(16)
 
     with engine.connect() as conn:
-        conn.execute(
-            text("""
-                insert into gold.labeling_sessions (token, break_id, session_date, sent_at)
-                values (:token, :break_id, :session_date, :sent_at)
-            """
-            ),
-            {
-                "token": token,
-                "break_id": BREAK_ID,
-                "session_date": date.today(),
-                "sent_at": datetime.now(),
-            }
-        )
+        try:
+            conn.execute(
+                text("""
+                    insert into gold.labeling_sessions (token, break_id, session_date, sent_at)
+                    values (:token, :break_id, :session_date, :sent_at)
+                """
+                ),
+                {
+                    "token": token,
+                    "break_id": BREAK_ID,
+                    "session_date": date.today(),
+                    "sent_at": datetime.now(),
+                }
+            )
 
-        conn.commit()
+            conn.commit()
+            logging.info("Data written")
+        except Exception as e:
+            logging.error(f"Write failed: {e}")
+            raise
 
     return token
+
+if __name__ == "__main__":
+    create_session()
