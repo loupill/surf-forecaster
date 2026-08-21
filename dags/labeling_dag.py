473419b83e60669b9a98f01b6dfec7ab2694e62e
louie pillari <loupillari@gmail.com>
2026-08-20 21:28:44 -0400

    update labeling dag for airflow

--- dags/labeling_dag.py
+++ dags/labeling_dag.py
@@ -3,20 +3,17 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 
 from create_session import create_session
+from daily_labeling_reminder import send_daily_labeling_reminder
 
 with DAG(
     dag_id = 'create_labeling_session',
     start_date=datetime(2026, 1, 1, tzinfo=timezone.utc),
-    schedule="0 20 * * *",
+    schedule="0 1 * * *",
     catchup=False,
     tags = ["surf", "labeling_app"]
 ) as dag:
  
-    create_session_task = PythonOperator(
-        task_id = "create_session",
-        python_callable = create_session
+    send_daily_reminder = PythonOperator(
+        task_id = "send_reminder",
+        python_callable = send_daily_labeling_reminder
     )
-
-
-if __name__ == "__main__":
-    dag.test()
