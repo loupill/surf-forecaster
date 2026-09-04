5a4941f8784e7dbd23176f9682cad6d75771f8a6
louie pillari <loupillari@gmail.com>
2026-09-04 10:51:43 -0400

    create function for pulling data to be used in llm summary

---
+++ src/llm_summary/summary_data_pull.py
@@ -0,0 +1,36 @@
+from ingest.db import get_engine
+from sqlalchemy import text
+import pandas as pd
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s",
+)
+
+scored_data = """
+select *
+from gold.scored_forecasts
+where retrieved_at = (select max(retrieved_at) from gold.scored_forecasts)
+"""
+
+def get_data(query):
+    '''
+    Get necessary data for creating summary
+    '''
+    engine = get_engine()
+
+    with engine.connect() as conn:
+        try:
+            df = pd.read_sql(text(query), conn)
+        except Exception as e:
+            logging.error(f"Read failed: {e}")
+            raise
+
+    return df
+
+
+
+if __name__ == "__main__":
+    df = get_data(scored_data)
+
